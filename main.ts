60f706cd9e5456935bc6817942d54eaf0de78cca
luusc <47896883+luusc@users.noreply.github.com>
2020-03-14 12:43:57 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.AB, function () {
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 4; y++) {
+            led.plot(x, y)
+            basic.pause(500)
+        }
+    }
+    music.playTone(932, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    music.playTone(131, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    for (let index = 0; index < 62; index++) {
+        led.plot(Math.randomRange(0, 4), Math.randomRange(0, 4))
+        basic.pause(100)
+    }
+    music.playTone(587, music.beat(BeatFraction.Whole))
+})
+basic.forever(function () {
+	
+})
