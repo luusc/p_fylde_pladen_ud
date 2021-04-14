b3ef74816fb3cbf76ecbd79e87815f63f38fbabc
luusc <47896883+luusc@users.noreply.github.com>
2021-04-14 08:26:55 +0200

    Opdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    for (let index = 0; index < 62; index++) {
+        led.plot(randint(0, 4), randint(0, 4))
+        basic.pause(100)
+    }
+})
 input.onButtonPressed(Button.AB, function () {
     for (let x = 0; x <= 4; x++) {
         for (let y = 0; y <= 4; y++) {
@@ -7,19 +14,4 @@ input.onButtonPressed(Button.AB, function () {
     }
     music.playTone(932, music.beat(BeatFraction.Whole))
 })
-input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
-    music.playTone(131, music.beat(BeatFraction.Half))
-})
-input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    for (let index = 0; index < 62; index++) {
-        led.plot(Math.randomRange(0, 4), Math.randomRange(0, 4))
-        basic.pause(100)
-    }
-    music.playTone(587, music.beat(BeatFraction.Whole))
-})
-basic.showString("Fylde pladen ud?")
-basic.forever(function () {
-	
-})
+basic.showString("?")
